5ea06a7fcb0ee3d4c74310f200338533f99b71e2
Vijay Kumar <vijaykumar_23se170@dtu.ac.in>
2024-10-02 12:18:59 +0530

    folder structures updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  
+ 
+
 
   return (
     <div>
